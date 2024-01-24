dd0e8a29815a0daee8f053a8669d29ada82e273d
Cloudy34 <Jenykumar.Patel@strathmore.edu>
2024-01-24 11:26:58 +0300

    first-comm

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Lists from './components/Lists'
+
+function App() {
+  return (
+    <div className="todo-app">
+       
+       <Lists />
+
+    </div>
+  );
+}
+
+export default App;
+// react Todo List App -- 43:00 min 
